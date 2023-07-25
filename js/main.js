76b41f948aec96953eca4cf41143ddfb2d0ab3ef
Pbemits <pbemits@gmail.com>
2023-07-26 04:06:19 +0700

    up soucer code sision one

---
+++ js/main.js
@@ -0,0 +1,33 @@
+// document.addEventListener("DOMContentLoaded", function() {
+// // Begin: code js Buytick of tour dates
+//     const buyBtns = document.querySelectorAll('.js-buy-ticket')
+//     const modal = document.querySelector('.js-modal')
+//     const modalClose = document.querySelector('.js-modal-close')
+//     const modalContainer = document.querySelector('.js-modal-container')
+
+// // Hàm hiển thị modal mua vé (thêm class open vào modal)
+// function showBuyTickets() {
+//     modal.classList.add('open')
+// }
+// // Hàm ẩn modal mua vé (gỡ bỏ class open của modal)
+// function removeBuyTickets() {
+//     modal.classList.remove('open')
+// }
+// // Lặp qua từng thẻ button và nghe hành vi click
+// for (const buyBtn of buyBtns) {
+//     buyBtn.addEventListener('click', showBuyTickets)
+// }
+// // Nghe hành vi click vào button close
+// modalClose.addEventListener('click', removeBuyTickets)
+
+// modal.addEventListener('click', removeBuyTickets)
+// modalContainer.addEventListener('click', function(event) {
+//     event.stopPropagation()
+// })
+// // End: code js Buytick of tour dates
+// // Begin: code js menu of header
+// var header = document.getElementById('header');
+// var mobileMenu = document.getElementById('mobile-menu');
+// console.log(mobileMenu);
+// // End: code js menu of header
+// });
